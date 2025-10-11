a1d854a648d430319dd5056b0e71734c8844a369
limlimlim00 <limlimlim1109@gmail.com>
2025-10-12 02:02:50 +0900

    add configs: troia

---
+++ configs/vid/temporal_roi_align/selsa-troialign_faster-rcnn_r101-dc5_1xb1-30e_ceusapvid.py
@@ -0,0 +1,7 @@
+_base_ = ['./selsa-troialign_faster-rcnn_r50-dc5_1xb1-30e_ceusapvid.py']
+model = dict(
+    detector=dict(
+        backbone=dict(
+            depth=101,
+            init_cfg=dict(
+                type='Pretrained', checkpoint='torchvision://resnet101'))))
