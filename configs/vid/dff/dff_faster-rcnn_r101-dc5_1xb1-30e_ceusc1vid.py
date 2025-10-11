e99c1945dcb53181055b56df1c2147bb28bfb218
limlimlim00 <limlimlim1109@gmail.com>
2025-10-11 18:05:50 +0900

    add confgis: [(dff, fgfa), 1 class]

---
+++ configs/vid/dff/dff_faster-rcnn_r101-dc5_1xb1-30e_ceusc1vid.py
@@ -0,0 +1,7 @@
+_base_ = ['./dff_faster-rcnn_r50-dc5_1xb1-30e_ceusc1vid.py']
+model = dict(
+    detector=dict(
+        backbone=dict(
+            depth=101,
+            init_cfg=dict(
+                type='Pretrained', checkpoint='torchvision://resnet101'))))
