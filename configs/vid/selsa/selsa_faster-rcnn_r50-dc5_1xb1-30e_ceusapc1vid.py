c952d93e1b4c9424b7e839a1a5eb08466b0095ef
limlimlim00 <limlimlim1109@gmail.com>
2025-10-12 01:46:12 +0900

    add configs: selsa

---
+++ configs/vid/selsa/selsa_faster-rcnn_r50-dc5_1xb1-30e_ceusapc1vid.py
@@ -0,0 +1,63 @@
+_base_ = [
+    '../../_base_/models/faster-rcnn_r50-dc5.py',
+    '../../_base_/datasets/ceus_ap_c1_vid_fgfa_style.py',
+    '../../_base_/default_runtime.py'
+]
+model = dict(
+    type='SELSA',
+    detector=dict(
+        roi_head=dict(
+            type='mmtrack.SelsaRoIHead',
+            bbox_head=dict(
+                type='mmtrack.SelsaBBoxHead',
+                num_shared_fcs=2,
+                aggregator=dict(
+                    type='mmtrack.SelsaAggregator',
+                    in_channels=1024,
+                    num_attention_blocks=16)),
+            bbox_roi_extractor=dict(
+                type='mmtrack.SingleRoIExtractor',
+                roi_layer=dict(
+                    type='RoIAlign', output_size=7, sampling_ratio=2),
+                out_channels=512,
+                featmap_strides=[16])
+        ),
+        test_cfg=dict(
+            rcnn=dict(
+                score_thr=1e-4,
+                nms=dict(type='nms', iou_threshold=0.5),
+                max_per_img=1
+            )
+        )
+    )
+)
+
+# training schedule
+train_cfg = dict(type='EpochBasedTrainLoop', max_epochs=30, val_interval=1)
+val_cfg = dict(type='ValLoop')
+test_cfg = dict(type='TestLoop')
+
+# learning rate
+param_scheduler = [
+    dict(
+        type='LinearLR',
+        start_factor=1.0 / 3,
+        by_epoch=False,
+        begin=0,
+        end=500),
+    dict(
+        type='MultiStepLR',
+        begin=0,
+        end=30,
+        by_epoch=True,
+        milestones=[12, 20, 26, 28],
+        gamma=0.2)
+]
+
+# optimizer
+optim_wrapper = dict(
+    type='OptimWrapper',
+    optimizer=dict(type='SGD', lr=0.01, momentum=0.9, weight_decay=0.0001),
+    clip_grad=dict(max_norm=35, norm_type=2))
+
+visualizer = dict(type='DetLocalVisualizer')
