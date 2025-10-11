434b3c1b0e16261191965179e34cd217da2965de
limlimlim00 <limlimlim1109@gmail.com>
2025-10-11 19:24:11 +0900

    add configs: AP-only configs

---
+++ configs/_base_/datasets/ceus_ap_vid_fgfa_style.py
@@ -0,0 +1,83 @@
+# dataset settings
+dataset_type = 'CeusVIDDataset'
+data_root = 'data/CEUS/'
+
+# data pipeline
+train_pipeline = [
+    dict(
+        type='TransformBroadcaster',
+        share_random_params=True,
+        transforms=[
+            dict(type='LoadImageFromFile'),
+            dict(type='LoadTrackAnnotations', with_instance_id=True),
+            dict(type='mmdet.Resize', scale=(512, 512), keep_ratio=False),
+        ]),
+    dict(type='PackTrackInputs')
+]
+test_pipeline = [
+    dict(
+        type='TransformBroadcaster',
+        share_random_params=True,
+        transforms=[
+            dict(type='LoadImageFromFile'),
+            dict(type='mmdet.Resize', scale=(512, 512), keep_ratio=False),
+        ]),
+    dict(type='PackTrackInputs', pack_single_img=False)
+]
+
+# dataloader
+train_dataloader = dict(
+    batch_size=1,
+    num_workers=2,
+    persistent_workers=True,
+    sampler=dict(type='DefaultSampler', shuffle=True),
+    dataset=dict(
+        type=dataset_type,                  
+        data_root=data_root,
+        ann_file='annotations/ceus_ap_train.json',
+        data_prefix=dict(img_path='Data'),     # file_name이 fold_k/... 시작
+        filter_cfg=dict(filter_empty_gt=False, min_size=1),  # blank 프레임 유지
+        pipeline=train_pipeline,
+        load_as_video=True,
+        key_img_sampler=dict(interval=1),       # CEUS: 모든 프레임을 key로 사용
+        ref_img_sampler=dict(
+            num_ref_imgs=11,
+            frame_range=[-11, 11],
+            filter_key_img=True,
+            method='all_inclusive'
+        ),
+    ),
+)
+
+val_dataloader = dict(
+    batch_size=1,
+    num_workers=2,
+    persistent_workers=True,
+    drop_last=False,
+    sampler=dict(type='VideoSampler'),
+    dataset=dict(
+        type=dataset_type,
+        data_root=data_root,
+        ann_file='annotations/ceus_ap_val.json',
+        data_prefix=dict(img_path='Data'),
+        pipeline=test_pipeline,
+        load_as_video=True,
+        ref_img_sampler=dict(
+            num_ref_imgs=11,
+            frame_range=[-11, 11],
+            filter_key_img=True,
+            method='all_inclusive'
+        ),
+        test_mode=True
+    ),
+)
+
+test_dataloader = val_dataloader
+
+# evaluator
+val_evaluator = dict(
+    type='CocoVideoMetric',
+    ann_file=data_root + 'annotations/ceus_ap_val.json',
+    metric='bbox')
+
+test_evaluator = val_evaluator
---
+++ configs/vid/fgfa/fgfa_faster-rcnn_r50-dc5_1xb1-30e_ceusapvid.py
@@ -0,0 +1,61 @@
+_base_ = [
+    '../../_base_/models/faster-rcnn_r50-dc5.py',
+    '../../_base_/datasets/ceus_ap_vid_fgfa_style.py',
+    '../../_base_/default_runtime.py'
+]
+model = dict(
+    type='FGFA',
+    detector=dict(
+        train_cfg=dict(
+            rpn_proposal=dict(max_per_img=300),
+            rcnn=dict(sampler=dict(num=256))),
+        test_cfg=dict(
+            rpn=dict(nms_pre=2000, max_per_img=400),
+            rcnn=dict(
+                score_thr=1e-4,
+                nms=dict(type='nms', iou_threshold=0.5),
+                max_per_img=1
+            )
+        )
+    ),
+    motion=dict(
+        type='FlowNetSimple',
+        img_scale_factor=1.0,
+        init_cfg=dict(
+            type='Pretrained',
+            checkpoint=  # noqa: E251
+            'https://download.openmmlab.com/mmtracking/pretrained_weights/flownet_simple.pth'  # noqa: E501
+        )),
+    aggregator=dict(
+        type='EmbedAggregator', num_convs=1, channels=512, kernel_size=3),
+)
+
+# training schedule
+train_cfg = dict(type='EpochBasedTrainLoop', max_epochs=30, val_interval=1)
+val_cfg = dict(type='ValLoop')
+test_cfg = dict(type='TestLoop')
+
+# learning rate
+param_scheduler = [
+    dict(
+        type='LinearLR',
+        start_factor=1.0 / 3,
+        by_epoch=False,
+        begin=0,
+        end=500),
+    dict(
+        type='MultiStepLR',
+        begin=0,
+        end=30,
+        by_epoch=True,
+        milestones=[12, 20, 26, 28],
+        gamma=0.2)
+]
+
+# optimizer
+optim_wrapper = dict(
+    type='OptimWrapper',
+    optimizer=dict(type='SGD', lr=0.01, momentum=0.9, weight_decay=0.0001),
+    clip_grad=dict(max_norm=35, norm_type=2))
+
+visualizer = dict(type='DetLocalVisualizer')
