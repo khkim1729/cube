0b4050eb3f4bbec5365c9d340eaeaa82fdeb04dc
limlimlim00 <limlimlim1109@gmail.com>
2025-12-04 12:48:13 +0900

    modify configs for visualization

--- configs/_base_/datasets/ceus_vid_dg_style.py
+++ configs/_base_/datasets/ceus_vid_dg_style.py
@@ -20,10 +20,16 @@ test_pipeline = [
         share_random_params=True,
         transforms=[
             dict(type='LoadImageFromFile'),
+            dict(type='LoadTrackAnnotations', with_instance_id=True),
             dict(type='mmdet.Resize', scale=(512, 512), keep_ratio=False),
         ]),
-    dict(type='PackTrackInputs', num_key_frames=16, meta_keys=('video_label', 'video_category', 'phase'),
-         pack_single_img=False)
+    dict(type='PackTrackInputs',
+         num_key_frames=16,
+         pack_single_img=False,
+         meta_keys=('img_id','img_path','ori_shape','img_shape','scale_factor',  # visualization 결과 저장을 위해 metadata 추가
+                    'video_id','frame_id','pid','fold','category','phase','is_aug',
+                    'video_label', 'video_category', 'phase')
+         )
 ]
 
 # dataloader
