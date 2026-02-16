049309a978745cbcfa15ac196168695a6157628e
limlimlim00 <limlimlim1109@gmail.com>
2026-02-16 02:46:23 +0000

    add visualizer

--- mmtrack/visualization/__init__.py
+++ mmtrack/visualization/__init__.py
@@ -2,5 +2,7 @@
 from .local_visualizer import DetLocalVisualizer, TrackLocalVisualizer
 from .local_overlay_visualizer import DetLocalVisualizerOverlay
 from .local_graph_overlay_visualizer import DetGraphLocalVisualizerOverlay
+from .overlay_visualizer import OverlayLocalVisualizer
 
-__all__ = ['TrackLocalVisualizer', 'DetLocalVisualizer', 'DetLocalVisualizerOverlay', 'DetGraphLocalVisualizerOverlay']
+__all__ = ['TrackLocalVisualizer', 'DetLocalVisualizer', 'DetLocalVisualizerOverlay',
+           'DetGraphLocalVisualizerOverlay', 'OverlayLocalVisualizer']
