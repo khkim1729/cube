621e37453b4071f5434536008d0b41e7ff06dd14
limlimlim00 <limlimlim1109@gmail.com>
2026-02-22 07:33:39 +0000

    rollback visualizers

--- mmtrack/visualization/__init__.py
+++ mmtrack/visualization/__init__.py
@@ -2,7 +2,5 @@
 from .local_visualizer import DetLocalVisualizer, TrackLocalVisualizer
 from .local_overlay_visualizer import DetLocalVisualizerOverlay
 from .local_graph_overlay_visualizer import DetGraphLocalVisualizerOverlay
-from .overlay_visualizer import OverlayLocalVisualizer
 
-__all__ = ['TrackLocalVisualizer', 'DetLocalVisualizer', 'DetLocalVisualizerOverlay',
-           'DetGraphLocalVisualizerOverlay', 'OverlayLocalVisualizer']
+__all__ = ['TrackLocalVisualizer', 'DetLocalVisualizer', 'DetLocalVisualizerOverlay', 'DetGraphLocalVisualizerOverlay']
